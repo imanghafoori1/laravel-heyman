586bbb9b5f731995213441dd7a72f0e263f59ac3
imanghafoori <imanghafoori1@gmail.com>
2018-07-06 23:45:16 +0430

    added test case class

---
+++ tests/TestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+abstract class TestCase extends Orchestra\Testbench\TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return ['Imanghafoori\HeyMan\HeyManServiceProvider'];
+    }
+}
