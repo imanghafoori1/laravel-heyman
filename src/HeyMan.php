c9c3ca89b3db2abebd6cf17158967f82d310f1b3
imanghafoori <imanghafoori1@gmail.com>
2018-07-07 20:28:42 +0430

    added visiting Url methods

--- src/HeyMan.php
+++ src/HeyMan.php
@@ -4,5 +4,23 @@ namespace Imanghafoori\HeyMan;
 
 class HeyMan
 {
+    private $urls = [];
+
+    private $target;
+
+    private $value;
+
+    public function whenVisitingUrl($url)
+    {
+        $this->value = $url;
+        $this->target = 'urls';
+
+        return $this;
+    }
+
+    public function getUrls()
+    {
+        return $this->urls;
+    }
 
 }
