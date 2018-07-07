df2d24ab122ea14f0e667d2246a070c9401ca1b0
imanghafoori <imanghafoori1@gmail.com>
2018-07-07 21:36:46 +0430

    Added findById method

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -3,8 +3,22 @@
 namespace Imanghafoori\HeyMan\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Imanghafoori\HeyMan\Utils\GuardManager;
 
 class Role extends Model
 {
     public $guarded = ['id'];
+
+    public static function findById(int $id, $guardName = null)
+    {
+        $guardName = $guardName ?? GuardManager::getDefaultName(static::class);
+
+        $role = static::where('id', $id)->where('guard_name', $guardName)->first();
+
+        if (! $role) {
+            throw RoleDoesNotExist::withId($id);
+        }
+
+        return $role;
+    }
 }
