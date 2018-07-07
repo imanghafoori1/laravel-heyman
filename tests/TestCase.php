e89b8666954c7ba9ac81383bc490d7ebf1d600d6
imanghafoori <imanghafoori1@gmail.com>
2018-07-07 20:23:46 +0430

    setting in memory database for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,4 +18,21 @@ abstract class TestCase extends Orchestra\Testbench\TestCase
 
         // and other test setup steps you need to perform
     }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
 }
