5f7e168bc8f1a44bec499b9ccd2ebf39848554be
imanghafoori <imanghafoori1@gmail.com>
2018-07-07 20:23:19 +0430

    adds database config for the test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,4 +6,16 @@ abstract class TestCase extends Orchestra\Testbench\TestCase
     {
         return ['Imanghafoori\HeyMan\HeyManServiceProvider'];
     }
+
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+
+        // and other test setup steps you need to perform
+    }
 }
