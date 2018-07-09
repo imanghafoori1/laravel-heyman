11d19395e8c61d26820c3d25711f7bffd115d3c7
imanghafoori <imanghafoori1@gmail.com>
2018-07-09 21:31:49 +0430

    Added first test for Event authorization

--- tests/EventsAuthorizationTest.php
+++ tests/EventsAuthorizationTest.php
@@ -1,6 +1,21 @@
 <?php
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Imanghafoori\HeyMan\Facades\HeyMan;
+
 class EventsAuthorizationTest extends TestCase
 {
+    public function testEventIsAuthorized1()
+    {
+        setUp::run($this);
+
+        HeyMan::whenEventHappens(['myEvent', 'myEvent1'])->youShouldHaveRole('reader')->beCareful();
+        HeyMan::whenEventHappens('myEvent4')->youShouldHaveRole('reader')->beCareful();
+
+        $this->expectException(AuthorizationException::class);
+
+        event('myEvent1');
+    }
+
 
 }
