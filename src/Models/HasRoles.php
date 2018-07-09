17788abaea5b13a28c4bb587acfb338b9caee09f
imanghafoori <imanghafoori1@gmail.com>
2018-07-09 16:24:40 +0430

    Added convert pipeToArray method

--- src/Models/HasRoles.php
+++ src/Models/HasRoles.php
@@ -70,4 +70,26 @@ trait HasRoles
         }
     }
 
+    protected function convertPipeToArray(string $pipeString)
+    {
+        $pipeString = trim($pipeString);
+
+        if (strlen($pipeString) <= 2) {
+            return $pipeString;
+        }
+
+        $quoteCharacter = substr($pipeString, 0, 1);
+        $endCharacter = substr($quoteCharacter, -1, 1);
+
+        if ($quoteCharacter !== $endCharacter) {
+            return explode('|', $pipeString);
+        }
+
+        if (! in_array($quoteCharacter, ["'", '"'])) {
+            return explode('|', $pipeString);
+        }
+
+        return explode('|', trim($pipeString, $quoteCharacter));
+    }
+
 }
