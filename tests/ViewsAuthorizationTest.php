696d9583a4987c5127f37b2c22c3a4f75bca8642
imanghafoori <imanghafoori1@gmail.com>
2018-07-09 21:23:24 +0430

    Added test for view authorization

--- tests/ViewsAuthorizationTest.php
+++ tests/ViewsAuthorizationTest.php
@@ -1,5 +1,30 @@
 <?php
 
-class ViewsAuthrizationTest
+use Illuminate\Auth\Access\AuthorizationException;
+use Imanghafoori\HeyMan\Facades\HeyMan;
+
+class ViewsAuthorizationTest extends TestCase
 {
+    public function testUrlIsAuthorized()
+    {
+        setUp::run($this);
+
+        HeyMan::whenYouSeeViewFile('welcome')->youShouldHaveRole('reader')->beCareful();
+
+        $this->expectException(AuthorizationException::class);
+
+        view('welcome');
+    }
+
+    public function testUrlIsAuthorized2134()
+    {
+        setUp::run($this);
+
+        HeyMan::whenYouSeeViewFile(['welcome'])->youShouldHaveRole('reader')->beCareful();
+        HeyMan::whenYouSeeViewFile(['welcome1'])->youShouldHaveRole('reader')->beCareful();
+
+        $this->expectException(AuthorizationException::class);
+
+        view('welcome');
+    }
 }
