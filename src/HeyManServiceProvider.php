422c62753aee3ba20ce815f2f31e08c8ceccf110
imanghafoori <imanghafoori1@gmail.com>
2018-07-09 16:22:14 +0430

    added role checker when visiting urls

--- src/HeyManServiceProvider.php
+++ src/HeyManServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace Imanghafoori\HeyMan;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Routing\Events\RouteMatched;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class HeyManServiceProvider extends ServiceProvider
@@ -9,6 +12,17 @@ class HeyManServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/migrations');
+
+        Route::matched(function (RouteMatched $eventObj) {
+            $currentUrl = $eventObj->route->uri;
+            $urls = $this->app['hey_man']->getUrls();
+
+            if (isset($urls[$currentUrl]['role'])) {
+                if (! auth()->user()->hasRole($urls[$currentUrl]['role'])) {
+                    throw new AuthorizationException();
+                }
+            }
+        });
     }
 
     public function register()
