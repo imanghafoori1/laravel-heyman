c4e1ca9e5eb2c61f13f38494c82aaf09323a17d0
imanghafoori <imanghafoori1@gmail.com>
2018-07-09 18:48:58 +0430

    Adding a failing test for creating models

--- tests/EloquentAuthorizationTest.php
+++ tests/EloquentAuthorizationTest.php
@@ -1,5 +1,19 @@
 <?php
 
-class EloquentAuthorizationTest
+use App\User;
+use Illuminate\Auth\Access\AuthorizationException;
+use Imanghafoori\HeyMan\Facades\HeyMan;
+
+class EloquentAuthorizationTest extends TestCase
 {
+    public function testCreatingModelsIsAuthorized()
+    {
+        setUp::run($this);
+
+        HeyMan::whenCreatingModel(User::class)->youShouldHaveRole('reader')->beCareful();
+
+        $this->expectException(AuthorizationException::class);
+
+        User::create(['name' => 'iman', 'email' => 'n@gmail.com', 'password' => bcrypt('a')]);
+    }
 }
