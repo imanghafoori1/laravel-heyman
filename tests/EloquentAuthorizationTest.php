1588582bdf791d8f097a11266f5627ba6b474f1e
imanghafoori <imanghafoori1@gmail.com>
2018-07-09 21:04:05 +0430

    added test for saving models

--- tests/EloquentAuthorizationTest.php
+++ tests/EloquentAuthorizationTest.php
@@ -42,4 +42,19 @@ class EloquentAuthorizationTest extends TestCase
         $user->name = 'sss';
         $user->save();
     }
+
+
+    public function testUrlIsAuthorized334()
+    {
+        setUp::run($this);
+        User::create(['name' => 'iman', 'email' => 'n@gmail.com', 'password' => bcrypt('a')]);
+
+        HeyMan::whenSavingModel(User::class)->youShouldHaveRole('reader')->beCareful();
+
+        $this->expectException(AuthorizationException::class);
+
+        $user = User::find(2);
+        $user->name = 'sss';
+        $user->save();
+    }
 }
