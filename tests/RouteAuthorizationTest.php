fe5da5510f124d09cff9f758ec05a46e665d5b39
imanghafoori <imanghafoori1@gmail.com>
2018-07-09 11:59:31 +0430

    adds url test

--- tests/RouteAuthorizationTest.php
+++ tests/RouteAuthorizationTest.php
@@ -1,5 +1,16 @@
 <?php
 
-class RouteAuthorizationTest
+use Imanghafoori\HeyMan\Facades\HeyMan;
+
+class RouteAuthorizationTest extends TestCase
 {
+    public function testUrlIsAuthorized()
+    {
+        setUp::run($this);
+
+        HeyMan::whenVisitingUrl('welcome')->youShouldHaveRole('reader')->beCareful();
+        HeyMan::whenVisitingUrl('welcome1')->youShouldHaveRole('writer')->beCareful();
+
+        $this->get('welcome')->assertStatus(403);
+    }
 }
