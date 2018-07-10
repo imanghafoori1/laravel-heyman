51a88ca478c644a8cdc95b11ae849fd6b3d72c3e
Iman Ghafoori <i.ghafouri@netbarg.com>
2018-07-10 14:31:51 +0430

    re-factor duplicated code

--- src/HeyMan.php
+++ src/HeyMan.php
@@ -19,47 +19,47 @@ class HeyMan
 
     public function whenVisitingUrl(...$url)
     {
-        return $this->authorizer->init('urls', $this->normalizeInput($url));
+        return $this->authorize('urls', $url);
     }
 
     public function whenVisitingRoute(...$routeName)
     {
-        return ($this->authorizer->init('routeNames', $this->normalizeInput($routeName)));
+        return $this->authorize('routeNames', $routeName);
     }
 
     public function whenCallingAction(...$action)
     {
-        return $this->authorizer->init('actions', $this->normalizeInput($action));
+        return $this->authorize('actions', $action);
     }
 
     public function whenCreatingModel(...$model)
     {
-        return $this->authorizer->init('creating', $this->normalizeInput($model));
+        return $this->authorize('creating', $model);
     }
 
     public function whenUpdatingModel(...$model)
     {
-        return $this->authorizer->init('updating', $this->normalizeInput($model));
+        return $this->authorize('updating', $model);
     }
 
     public function whenSavingModel(...$model)
     {
-        return $this->authorizer->init('saving', $this->normalizeInput($model));
+        return $this->authorize('saving', $model);
     }
 
     public function whenDeletingModel(...$model)
     {
-        return $this->authorizer->init('deleting', $this->normalizeInput($model));
+        return $this->authorize('deleting', $model);
     }
 
     public function whenYouSeeViewFile(...$view)
     {
-        return $this->authorizer->init('views', $this->normalizeInput($view));
+        return $this->authorize('views', $view);
     }
 
     public function whenEventHappens(...$event)
     {
-        return $this->authorizer->init('events', $this->normalizeInput($event));
+        return $this->authorize('events', $event);
     }
 
     /**
@@ -69,5 +69,14 @@ class HeyMan
     private function normalizeInput($url): array
     {
         return is_array($url[0]) ? $url[0] : $url;
+    }
+
+    /**
+     * @param $url
+     * @return $this
+     */
+    private function authorize($target, $url): ConditionApplier
+    {
+        return $this->authorizer->init($target, $this->normalizeInput($url));
     }
 }
