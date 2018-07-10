db91767029499163cc88534e3ed52c9620922d68
Iman Ghafoori <i.ghafouri@netbarg.com>
2018-07-10 14:15:51 +0430

    remove redundant code

--- src/HeyMan.php
+++ src/HeyMan.php
@@ -4,12 +4,6 @@ namespace Imanghafoori\HeyMan;
 
 class HeyMan
 {
-    private $target;
-
-    private $value = [];
-
-    private $events = [];
-
     /**
      * @var \Imanghafoori\HeyMan\ConditionApplier
      */
@@ -25,91 +19,47 @@ class HeyMan
 
     public function whenVisitingUrl(...$url)
     {
-        $this->setValue($url);
-
-        $this->target = 'urls';
-        $t = $this->value;
-        $this->value = [];
-        return $this->authorizer->init('urls', $t);
+        return $this->authorizer->init('urls', $this->normalizeInput($url));
     }
 
     public function whenVisitingRoute(...$routeName)
     {
-        $this->setValue($routeName);
-
-        $this->target = 'routeNames';
-        $t = $this->value;
-        $this->value = [];
-        return ($this->authorizer->init('routeNames', $t));
+        return ($this->authorizer->init('routeNames', $this->normalizeInput($routeName)));
     }
 
     public function whenCallingAction(...$action)
     {
-        $this->setValue($action);
-
-        $this->target = 'actions';
-        $t = $this->value;
-        $this->value = [];
-        return $this->authorizer->init('actions', $t);
+        return $this->authorizer->init('actions', $this->normalizeInput($action));
     }
 
     public function whenCreatingModel(...$model)
     {
-        $this->setValue($model);
-
-        $this->target = 'creating';
-
-        return $this->authorizer->init('creating', $this->value);
+        return $this->authorizer->init('creating', $this->normalizeInput($model));
     }
 
     public function whenUpdatingModel(...$model)
     {
-        $this->setValue($model);
-
-        $this->target = 'updating';
-
-        return $this->authorizer->init('updating', $this->value);
+        return $this->authorizer->init('updating', $this->normalizeInput($model));
     }
 
     public function whenSavingModel(...$model)
     {
-        $this->setValue($model);
-
-        $this->target = 'saving';
-
-        return $this->authorizer->init('saving', $this->value);
+        return $this->authorizer->init('saving', $this->normalizeInput($model));
     }
 
     public function whenDeletingModel(...$model)
     {
-        $this->setValue($model);
-
-        $this->target = 'deleting';
-
-        return $this->authorizer->init('deleting', $this->value);
+        return $this->authorizer->init('deleting', $this->normalizeInput($model));
     }
 
     public function whenYouSeeViewFile(...$view)
     {
-        $this->setValue($view);
-
-        $this->target = 'views';
-
-        return $this->authorizer->init('views', $this->value);
+        return $this->authorizer->init('views', $this->normalizeInput($view));
     }
 
     public function whenEventHappens(...$event)
     {
-        $this->setValue($event);
-
-        $this->target = 'events';
-
-        return $this->authorizer->init('events', $this->value);
-    }
-
-    public function getEvents()
-    {
-        return $this->events;
+        return $this->authorizer->init('events', $this->normalizeInput($event));
     }
 
     /**
@@ -119,14 +69,5 @@ class HeyMan
     private function normalizeInput($url): array
     {
         return is_array($url[0]) ? $url[0] : $url;
-    }
-
-    /**
-     * @param $model
-     */
-    private function setValue($model)
-    {
-        $model = $this->normalizeInput($model);
-        $this->value = array_merge($this->value, $model);
     }
 }
