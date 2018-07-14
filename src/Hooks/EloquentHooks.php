1c326cf389fba8204cbcae7acc3d7703a5cbd2fb
imanghafoori <imanghafoori1@gmail.com>
2018-07-14 21:48:22 +0430

    change Api

--- src/Hooks/EloquentHooks.php
+++ src/Hooks/EloquentHooks.php
@@ -4,31 +4,31 @@ namespace Imanghafoori\HeyMan\Hooks;
 
 trait EloquentHooks
 {
-    public function whenFetchingModel(...$model)
+    public function whenFetching(...$model)
     {
         $model = $this->normalizeModel('retrieved', $model);
         return $this->authorize($model);
     }
 
-    public function whenCreatingModel(...$model)
+    public function whenCreating(...$model)
     {
         $model = $this->normalizeModel('creating', $model);
         return $this->authorize($model);
     }
 
-    public function whenUpdatingModel(...$model)
+    public function whenUpdating(...$model)
     {
         $model = $this->normalizeModel('updating', $model);
         return $this->authorize($model);
     }
 
-    public function whenSavingModel(...$model)
+    public function whenSaving(...$model)
     {
         $model = $this->normalizeModel('saving', $model);
         return $this->authorize($model);
     }
 
-    public function whenDeletingModel(...$model)
+    public function whenDeleting(...$model)
     {
         $model = $this->normalizeModel('deleting', $model);
         return $this->authorize($model);
