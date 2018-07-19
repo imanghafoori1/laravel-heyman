5dda34c6e392ba5e936e13445cefe193697b4949
imanghafoori <imanghafoori1@gmail.com>
2018-07-19 19:14:44 +0430

    Remove User models from tests directory

--- tests/EloquentAuthorizationTest.php
+++ tests/EloquentAuthorizationTest.php
@@ -11,22 +11,22 @@ class EloquentAuthorizationTest extends TestCase
     {
         setUp::run();
 
-        HeyMan::whenYouCreate(User::class)->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
-        HeyMan::whenYouCreate(User2::class)->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
+        HeyMan::whenYouCreate('\App\User')->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
+        HeyMan::whenYouCreate('\App\User2')->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
 
         $this->expectException(AuthorizationException::class);
 
-        event('eloquent.creating: '.User::class);
+        event('eloquent.creating: '.'\App\User');
     }
 
     public function testUpdatingModelsIsAuthorized()
     {
         setUp::run();
 
-        HeyMan::whenYouUpdate(User::class)->youShouldHaveRole('writer')->otherwise()->weDenyAccess();
-        HeyMan::whenYouCreate(User2::class)->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
+        HeyMan::whenYouUpdate('\App\User')->youShouldHaveRole('writer')->otherwise()->weDenyAccess();
+        HeyMan::whenYouCreate('\App\User2')->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
 
-        event('eloquent.updating: '.User::class);
+        event('eloquent.updating: '.'\App\User');
         $this->assertTrue(true);
     }
 
@@ -34,77 +34,77 @@ class EloquentAuthorizationTest extends TestCase
     {
         setUp::run();
 
-        HeyMan::whenYouUpdate(User::class)->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
+        HeyMan::whenYouUpdate('\App\User')->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
 
         $this->expectException(AuthorizationException::class);
 
-        event('eloquent.updating: '.User::class);
+        event('eloquent.updating: '.'\App\User');
     }
 
     public function testSavingModelsIsAuthorized2()
     {
         setUp::run();
 
-        HeyMan::whenYouSave(User::class)->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
+        HeyMan::whenYouSave('\App\User')->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
 
         $this->expectException(AuthorizationException::class);
 
-        event('eloquent.saving: '.User::class);
+        event('eloquent.saving: '.'\App\User');
     }
 
     public function testSavingModelsIsAuthorized3()
     {
         setUp::run();
 
-        HeyMan::whenYouSave(User::class)->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
+        HeyMan::whenYouSave('\App\User')->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
 
         $this->expectException(AuthorizationException::class);
 
-        event('eloquent.saving: '.User::class);
+        event('eloquent.saving: '.'\App\User');
     }
 
     public function testSavingModelsIsAuthorized1()
     {
         setUp::run();
 
-        HeyMan::whenYouSave(User::class)->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
+        HeyMan::whenYouSave('\App\User')->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
 
         $this->expectException(AuthorizationException::class);
 
-        event('eloquent.saving: '.User::class);
+        event('eloquent.saving: '.'\App\User');
     }
 
     public function testDeletingModelsIsAuthorized1()
     {
         setUp::run();
 
-        HeyMan::whenYouDelete(User::class)->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
+        HeyMan::whenYouDelete('\App\User')->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
 
         $this->expectException(AuthorizationException::class);
 
-        event('eloquent.deleting: '.User::class);
+        event('eloquent.deleting: '.'\App\User');
     }
 
     public function testDeletingModelsIsAuthorized2()
     {
         setUp::run();
 
-        HeyMan::whenYouDelete([User::class])->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
+        HeyMan::whenYouDelete(['\App\User'])->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
 
         $this->expectException(AuthorizationException::class);
 
-        event('eloquent.deleting: '.User::class);
+        event('eloquent.deleting: '.'\App\User');
     }
 
     public function testFetchingModelsIsAuthorized()
     {
         setUp::run();
 
-        HeyMan::whenYouFetch(User::class)->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
-        HeyMan::whenYouCreate(User2::class)->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
+        HeyMan::whenYouFetch('\App\User')->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
+        HeyMan::whenYouCreate('\App\User2')->youShouldHaveRole('reader')->otherwise()->weDenyAccess();
 
         $this->expectException(AuthorizationException::class);
 
-        event('eloquent.retrieved: '.User::class);
+        event('eloquent.retrieved: '.'\App\User');
     }
 }
