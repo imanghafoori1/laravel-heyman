c7cdc95f4b03461bf1e09dc9b2b859258e704d44
imanghafoori <imanghafoori1@gmail.com>
2018-07-19 12:57:52 +0430

    Added tests for afterCalling() feature

---
+++ tests/MethodCallTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use Imanghafoori\HeyMan\Facades\HeyMan;
+
+class MethodCallTest extends TestCase
+{
+    public function testُDeletingModelsIsAuthorized1()
+    {
+        setUp::run($this);
+
+        \Facades\Logger::shouldReceive('info')->once();
+
+        HeyMan::whenYouVisitUrl(['welcome', 'welcome_'])
+            ->youShouldHaveRole('writer')->otherwise()
+            ->afterCalling('Logger@info')->weDenyAccess();
+
+        $this->get('welcome');
+    }
+
+    public function testُDeletingsIsAuthorized1()
+    {
+
+        setUp::run($this);
+
+        $this->withoutExceptionHandling();
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('You have Called me');
+
+        $cb = function () {
+            throw new \Exception('You have Called me');
+        };
+        HeyMan::whenYouVisitUrl(['welcome', 'welcome_'])
+            ->youShouldHaveRole('writer')->otherwise()
+            ->afterCalling($cb)->weDenyAccess();
+
+        $this->get('welcome');
+    }
+}
