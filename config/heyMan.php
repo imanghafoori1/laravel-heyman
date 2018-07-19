a08beb30901ea8edb21d3ee13223615da879c672
imanghafoori <imanghafoori1@gmail.com>
2018-07-19 16:31:13 +0430

    remove extra configs

--- config/heyMan.php
+++ config/heyMan.php
@@ -1,23 +1,3 @@
 <?php
-
 return [
-    
-    'table_names' => [
-
-        /*
-         * When using the "HasRoles" trait from this package, we need to know which
-         * table should be used to retrieve your roles. We have chosen a basic
-         * default value but you may easily change it to any table you like.
-         */
-
-        'roles' => 'roles',
-
-        /*
-         * When using the "HasRoles" trait from this package, we need to know which
-         * table should be used to retrieve your models roles. We have chosen a
-         * basic default value but you may easily change it to any table you like.
-         */
-
-        'model_has_roles' => 'model_has_roles',
-    ],
 ];
