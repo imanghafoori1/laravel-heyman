acc0eb5776b92102f0422a6dffefcad4067328f2
imanghafoori <imanghafoori1@gmail.com>
2018-07-19 09:35:06 +0430

    Added docblocks

--- src/Hooks/RouteHooks.php
+++ src/Hooks/RouteHooks.php
@@ -2,8 +2,14 @@
 
 namespace Imanghafoori\HeyMan\Hooks;
 
+use Imanghafoori\HeyMan\YouShouldHave;
+
 trait RouteHooks
 {
+    /**
+     * @param mixed ...$url
+     * @return YouShouldHave
+     */
     public function whenYouVisitUrl(...$url)
     {
         $removeSlash = function ($url) {
@@ -15,17 +21,26 @@ trait RouteHooks
         return $this->authorizeRoute('urls', $url);
     }
 
+    /**
+     * @param mixed ...$routeName
+     * @return YouShouldHave
+     */
     public function whenYouVisitRoute(...$routeName)
     {
         return $this->authorizeRoute('routeNames', $this->normalizeInput($routeName));
     }
 
+    /**
+     * @param mixed ...$action
+     * @return YouShouldHave
+     */
     public function whenYouCallAction(...$action)
     {
         $addNamespace = function ($action) {
             if ($action = ltrim($action, '\\')) {
                 return $action;
             }
+
             return app()->getNamespace().'\\Http\\Controllers\\'.$action;
         };
 
@@ -33,10 +48,15 @@ trait RouteHooks
 
         return $this->authorizeRoute('actions', $action);
     }
-
+    /**
+     * @param $target
+     * @param $value
+     * @return YouShouldHave
+     */
     private function authorizeRoute($target, $value)
     {
         $this->authorizer = app('hey_man_route_authorizer')->init($target, $value);
-        return app(\Imanghafoori\HeyMan\YouShouldHave::class);
+
+        return app(YouShouldHave::class);
     }
 }
