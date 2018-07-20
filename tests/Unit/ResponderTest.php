987e03ace76b4f8afb6498e7f958111689b2d658
imanghafoori <imanghafoori1@gmail.com>
2018-07-20 13:56:16 +0430

    refactored tests

--- tests/Unit/ResponderTest.php
+++ tests/Unit/ResponderTest.php
@@ -2,49 +2,21 @@
 
 class ResponderTest extends TestCase
 {
-    public function testView()
-    {
-        $responder = new \Imanghafoori\HeyMan\Responder();
-        \Imanghafoori\HeyMan\Facades\HeyMan::shouldReceive('startListening')->once()->with(["view", ["welcome"]], null);
-
-        $responder->view('welcome');
-    }
-
-    public function testjsonp()
-    {
-        $responder = new \Imanghafoori\HeyMan\Responder();
-        \Imanghafoori\HeyMan\Facades\HeyMan::shouldReceive('startListening')->once()->with(["jsonp", ["welcome"]], null);
-
-        $responder->jsonp('welcome');
-    }
-
-    public function testdownload()
-    {
-        $responder = new \Imanghafoori\HeyMan\Responder();
-        \Imanghafoori\HeyMan\Facades\HeyMan::shouldReceive('startListening')->once()->with(["download", ["welcome"]], null);
-
-        $responder->download('welcome');
-    }
-
-    public function tesJson()
-    {
-        $responder = new \Imanghafoori\HeyMan\Responder();
-        \Imanghafoori\HeyMan\Facades\HeyMan::shouldReceive('startListening')->once()->with(["json", ["welcome"]], null);
-
-        $responder->json('welcome');
-    }
-
-    public function testStream()
-    {
-        $responder = new \Imanghafoori\HeyMan\Responder();
-        \Imanghafoori\HeyMan\Facades\HeyMan::shouldReceive('startListening')->once()->with(["stream", ["welcome"]], null);
-
-        $responder->stream('welcome');
-    }
-
     public function testRedirectToIntended()
     {
-        $methods = ['redirectToIntended', 'redirectToAction', 'redirectToRoute', 'redirectGuest'];
+        $methods = [
+            'redirectToIntended',
+            'redirectToAction',
+            'redirectToRoute',
+            'redirectGuest',
+            'stream',
+            'streamDownload',
+            'view',
+            'make',
+            'json',
+            'jsonp',
+            'download',
+        ];
         foreach ($methods as $method) {
             $param = str_random(3);
             $responder = new \Imanghafoori\HeyMan\Responder();
