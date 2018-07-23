17f2e9def1d7d1e325b893a334614db9f104e09a
imanghafoori <imanghafoori1@gmail.com>
2018-07-23 20:14:33 +0430

    Added tests for redirector class

--- tests/Unit/ResponderTest.php
+++ tests/Unit/ResponderTest.php
@@ -40,4 +40,38 @@ class ResponderTest extends TestCase
             $responder->{$method}($param);
         }
     }
+
+    public function testRedirectWith()
+    {
+        $methods2 = [
+            'with',
+            'withCookies',
+            'withInput',
+            'onlyInput',
+            'exceptInput',
+            'withErrors',
+        ];
+
+        $methods = [
+            'redirectToIntended',
+            'redirectToAction',
+            'redirectToRoute',
+            'redirectGuest',
+        ];
+
+        foreach ($methods2 as $method2) {
+            foreach ($methods as $method) {
+                $param = str_random(3);
+                $responder = new \Imanghafoori\HeyMan\Responder();
+                \Imanghafoori\HeyMan\Facades\HeyMan::shouldReceive('startListening')->once()->with([
+                    [$method, [$param]],
+                    [$method2, [['key', 'value']]],
+                    ['with', [['a', 'b']]],
+                ], null);
+
+                $responder->{$method}($param)->{$method2}(['key', 'value'])->with(['a', 'b']);
+            }
+        }
+    }
+
 }
