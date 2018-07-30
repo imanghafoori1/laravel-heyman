50c70b3baf488fa37d1adc96b4763a376b8a3d14
imanghafoori <imanghafoori1@gmail.com>
2018-07-30 21:06:07 +0430

    Added closure execution with ignorance

--- src/Consider.php
+++ src/Consider.php
@@ -11,24 +11,24 @@ class Consider
         $this->mode = $mode;
     }
 
-    public function eloquentChecks()
+    public function eloquentChecks(callable $closure = null)
     {
-        $this->ignore('heyman_ignore_eloquent');
+        $this->turn('heyman_ignore_eloquent', $closure);
     }
 
-    public function viewChecks()
+    public function viewChecks(callable $closure = null)
     {
-        $this->ignore('heyman_ignore_view');
+        $this->turn('heyman_ignore_view', $closure);
     }
 
-    public function routeChecks()
+    public function routeChecks(callable $closure = null)
     {
-        $this->ignore('heyman_ignore_route');
+        $this->turn('heyman_ignore_route', $closure);
     }
 
-    public function eventChecks()
+    public function eventChecks(callable $closure = null)
     {
-        $this->ignore('heyman_ignore_event');
+        $this->turn('heyman_ignore_event', $closure);
     }
 
     public function allChecks()
@@ -41,12 +41,20 @@ class Consider
 
     /**
      * @param $key
+     * @param callable|null $closure
      */
-    private function ignore($key)
+    private function turn($key, callable $closure = null)
     {
+        $current = config($key);
         config()->set($key, [
             'turnOff' => true,
             'turnOn'  => false,
         ][$this->mode]);
+
+        if (is_null($closure)) {
+            return ;
+        }
+        $closure();
+        config()->set($key, $current);
     }
 }
