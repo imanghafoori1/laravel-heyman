e402c0d60fd62de114266b4989471dfdd63c5e2e
Iman <imanghafoori1@gmail.com>
2018-08-10 12:05:31 +0000

    Apply fixes from StyleCI

--- src/YouShouldHave.php
+++ src/YouShouldHave.php
@@ -103,9 +103,10 @@ class YouShouldHave
     /**
      * Validate the given request with the given rules.
      *
-     * @param  array $rules
-     * @param  array $messages
-     * @param  array $customAttributes
+     * @param array $rules
+     * @param array $messages
+     * @param array $customAttributes
+     *
      * @return \Imanghafoori\HeyMan\Reactions\Validator
      */
     public function yourRequestShouldBeValid($rules, array $messages = [], array $customAttributes = []): Validator
