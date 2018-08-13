5db8293fcbb24a63f63f2238d25c2e42b41fddf2
imanghafoori <imanghafoori1@gmail.com>
2018-08-13 19:34:44 +0430

    remove extra code

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,30 +6,4 @@ abstract class TestCase extends Orchestra\Testbench\TestCase
     {
         return ['Imanghafoori\HeyMan\HeyManServiceProvider'];
     }
-
-    /**
-     * Setup the test environment.
-     */
-    protected function setUp()
-    {
-        parent::setUp();
-
-        // $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-
-        // and other test setup steps you need to perform
-    }
-
-    /**
-     * Define environment setup.
-     *
-     * @param \Illuminate\Foundation\Application $app
-     *
-     * @return void
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        // Setup default database to use sqlite :memory:
-        // $app['config']->set('database.default', 'testbench');
-        // $app['config']->set('database.connections.testbench', [ 'driver'   => 'sqlite', 'database' => ':memory:', 'prefix'   => '', ]);
-    }
 }
