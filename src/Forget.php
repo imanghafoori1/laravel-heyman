dbce1e871fc172c4b9ca17fa51f0bbca8f59ffa8
Iman <imanghafoori1@gmail.com>
2018-08-16 21:04:13 +0000

    Apply fixes from StyleCI

--- src/Forget.php
+++ src/Forget.php
@@ -2,8 +2,8 @@
 
 namespace Imanghafoori\HeyMan;
 
-use Imanghafoori\HeyMan\Normilizers\RouteNormalizer;
 use Imanghafoori\HeyMan\Normilizers\InputNormalizer;
+use Imanghafoori\HeyMan\Normilizers\RouteNormalizer;
 use Imanghafoori\HeyMan\WatchingStrategies\EloquentEventsManager;
 use Imanghafoori\HeyMan\WatchingStrategies\EventManager;
 use Imanghafoori\HeyMan\WatchingStrategies\RouterEventManager;
