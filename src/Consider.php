b9cd6ea92ffc51c0d7b74d8b7cef1922d668a1b5
Imanghafoori <imanghafoori1@gmail.com>
2018-08-20 10:51:29 +0430

    Grouped imports

--- src/Consider.php
+++ src/Consider.php
@@ -2,10 +2,7 @@
 
 namespace Imanghafoori\HeyMan;
 
-use Imanghafoori\HeyMan\WatchingStrategies\EloquentEventsManager;
-use Imanghafoori\HeyMan\WatchingStrategies\EventManager;
-use Imanghafoori\HeyMan\WatchingStrategies\RouterEventManager;
-use Imanghafoori\HeyMan\WatchingStrategies\ViewEventManager;
+use Imanghafoori\HeyMan\WatchingStrategies\{EloquentEventsManager, EventManager, RouterEventManager, ViewEventManager};
 
 class Consider
 {
