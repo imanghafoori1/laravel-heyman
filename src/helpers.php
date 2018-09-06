762c49aa5d6313a701e5b025e04385f49178d90a
imanghafoori <imanghafoori1@gmail.com>
2018-09-06 21:49:01 +0430

    seek backward compatibility with elder version of laravel

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,14 @@
 <?php
+
+if (! function_exists('resolve')) {
+    /**
+     * Resolve a service from the container.
+     *
+     * @param  string  $name
+     * @return mixed
+     */
+    function resolve($name)
+    {
+        return app($name);
+    }
+}
