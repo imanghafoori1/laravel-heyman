f21c32bf9bd0a7d18e09eaeb30db2a1882ccce27
imanghafoori <imanghafoori1@gmail.com>
2018-09-07 00:31:39 +0430

    clean up code

--- src/Conditions/ConditionsFacade.php
+++ src/Conditions/ConditionsFacade.php
@@ -2,25 +2,8 @@
 
 namespace Imanghafoori\HeyMan\Conditions;
 
-use Imanghafoori\HeyMan\Chain;
-
 class ConditionsFacade
 {
-    /**
-     * @var Chain
-     */
-    private $chain;
-
-    /**
-     * YouShouldHave constructor.
-     *
-     * @param Chain $chain
-     */
-    public function __construct(Chain $chain)
-    {
-        $this->chain = $chain;
-    }
-
     use \Imanghafoori\HeyMan\Conditions\Authentication;
     use \Imanghafoori\HeyMan\Conditions\Gate;
     use \Imanghafoori\HeyMan\Conditions\Callbacks;
