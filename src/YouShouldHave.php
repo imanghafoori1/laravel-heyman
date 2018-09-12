a79e48ff209f795ba24623836154976589fa210d
imanghafoori <imanghafoori1@gmail.com>
2018-09-12 21:46:45 +0430

    fixed return types

--- src/YouShouldHave.php
+++ src/YouShouldHave.php
@@ -2,22 +2,23 @@
 
 namespace Imanghafoori\HeyMan;
 
-use Imanghafoori\HeyMan\Conditions\ConditionsFacade;
+use Imanghafoori\HeyMan\Conditions\{ConditionsFacade, RequestValidation};
+use Imanghafoori\HeyMan\Reactions\Reactions;
 
 final class YouShouldHave
 {
-    use \Imanghafoori\HeyMan\Conditions\RequestValidation;
+    use RequestValidation;
 
     public $condition;
 
-    public function __call($method, $args)
+    public function __call($method, $args): Otherwise
     {
         resolve(Chain::class)->condition = app(ConditionsFacade::class)->$method(...$args);
 
         return resolve(Otherwise::class);
     }
 
-    public function always()
+    public function always() : Reactions
     {
         return $this->thisValueShouldAllow(false)->otherwise();
     }
