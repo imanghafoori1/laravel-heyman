a1d459ae9a070588ae11084747e58831e77b1c1c
Iman <imanghafoori1@gmail.com>
2018-10-12 15:19:44 +0000

    Apply fixes from StyleCI

--- src/Situations/SituationsProxy.php
+++ src/Situations/SituationsProxy.php
@@ -2,7 +2,10 @@
 
 namespace Imanghafoori\HeyMan\Situations;
 
-use Imanghafoori\HeyMan\WatchingStrategies\{EloquentModels\EloquentSituations, Events\EventSituations, Routes\RouteSituations, Views\ViewSituations};
+use Imanghafoori\HeyMan\WatchingStrategies\EloquentModels\EloquentSituations;
+use Imanghafoori\HeyMan\WatchingStrategies\Events\EventSituations;
+use Imanghafoori\HeyMan\WatchingStrategies\Routes\RouteSituations;
+use Imanghafoori\HeyMan\WatchingStrategies\Views\ViewSituations;
 use Imanghafoori\HeyMan\YouShouldHave;
 
 final class SituationsProxy
