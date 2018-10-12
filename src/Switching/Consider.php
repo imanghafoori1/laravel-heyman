c88fe1c123105a6de633498b459cd323b31c2a5d
imanghafoori <imanghafoori1@gmail.com>
2018-10-12 19:21:52 +0330

    group imports

--- src/Switching/Consider.php
+++ src/Switching/Consider.php
@@ -2,10 +2,7 @@
 
 namespace Imanghafoori\HeyMan\Switching;
 
-use Imanghafoori\HeyMan\WatchingStrategies\EloquentModels\EloquentEventsManager;
-use Imanghafoori\HeyMan\WatchingStrategies\Events\EventManager;
-use Imanghafoori\HeyMan\WatchingStrategies\Routes\RouterEventManager;
-use Imanghafoori\HeyMan\WatchingStrategies\Views\ViewEventManager;
+use Imanghafoori\HeyMan\WatchingStrategies\{EloquentModels\EloquentEventsManager, Events\EventManager, Routes\RouterEventManager, Views\ViewEventManager};
 
 /**
  * Class Consider.
