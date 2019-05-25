b8aa115d646f8ff9d08f5b1116e5895567ce06c8
imanghafoori <imanghafoori1@gmail.com>
2019-05-26 00:38:05 +0430

    fix a bug

--- src/Reactions/Responder.php
+++ src/Reactions/Responder.php
@@ -5,13 +5,13 @@ namespace Imanghafoori\HeyMan\Reactions;
 /**
  * Class Responder.
  *
- * @method null make(string $content = '', int $status = 200, array $headers = [])
- * @method null view(string $view, array $data = [], int $status = 200, array $headers = [])
- * @method null json($data = [], $status = 200, array $headers = [], $options = 0)
- * @method null jsonp($callback, $data = [], $status = 200, array $headers = [], $options = 0)
- * @method null stream($callback, $status = 200, array $headers = [])
- * @method null streamDownload($callback, $name = null, array $headers = [], $disposition = 'attachment')
- * @method null download($file, $name = null, array $headers = [], $disposition = 'attachment')
+ * @method Then make(string $content = '', int $status = 200, array $headers = [])
+ * @method Then view(string $view, array $data = [], int $status = 200, array $headers = [])
+ * @method Then json($data = [], $status = 200, array $headers = [], $options = 0)
+ * @method Then jsonp($callback, $data = [], $status = 200, array $headers = [], $options = 0)
+ * @method Then stream($callback, $status = 200, array $headers = [])
+ * @method Then streamDownload($callback, $name = null, array $headers = [], $disposition = 'attachment')
+ * @method Then download($file, $name = null, array $headers = [], $disposition = 'attachment')
  */
 final class Responder
 {
@@ -30,5 +30,7 @@ final class Responder
     public function __call($method, $parameters)
     {
         resolve('heyman.chain')->push('data', [$method, $parameters]);
+
+        return new Then($this->action);
     }
 }
