b9586a771cd0e754769136fe3d9925425df87a71
imanghafoori <imanghafoori1@gmail.com>
2019-05-26 00:55:38 +0430

    added test for bug fix

---
+++ tests/TerminateWithTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Imanghafoori\HeyMan\StartGuarding;
+use Imanghafoori\HeyMan\Facades\HeyMan;
+use Imanghafoori\MakeSure\Facades\MakeSure;
+
+class TerminateWithTest extends TestCase
+{
+    public function testPutUrls()
+    {
+        Route::put('/put', function () {
+        });
+
+        HeyMan::whenYouSendPut('put')->always()->response()->json(['hello' => 'bye'], 301)
+            ->then()
+            ->terminateWith(function () {
+                event('terminated_well');
+            });
+        $this->expectsEvents('terminated_well');
+
+        app(StartGuarding::class)->start();
+        $this->put('put')->assertExactJson(['hello' => 'bye'])->assertStatus(301);
+    }
+
+}
