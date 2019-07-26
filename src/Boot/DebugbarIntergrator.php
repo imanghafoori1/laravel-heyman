050109125db4286bdd413aba0ecb8c42258c1168
imanghafoori <imanghafoori1@gmail.com>
2019-07-26 08:33:09 +0430

    DEBUG.

--- src/Boot/DebugbarIntergrator.php
+++ src/Boot/DebugbarIntergrator.php
@@ -17,9 +17,7 @@ class DebugbarIntergrator
         app()->make('debugbar')->addCollector(app('heyman.debugger'));
 
         \Event::listen('heyman_reaction_is_happening', function (...$debug) {
-            resolve('heyman.debugger')->addMessage('HeyMan Rule Matched in file: '.$debug[0]);
-            resolve('heyman.debugger')->addMessage('on line: '.$debug[1]);
-            resolve('heyman.debugger')->addMessage($debug[2]);
+            app()['heyman_reaction_is_happened_in_debug'] = $debug;
         });
     }
 }
--- src/WatchingStrategies/Routes/MatchedRoute.php
+++ src/WatchingStrategies/Routes/MatchedRoute.php
@@ -5,6 +5,7 @@ namespace Imanghafoori\HeyMan\WatchingStrategies\Routes;
 use Closure;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
+use Illuminate\Http\RedirectResponse;
 
 class MatchedRoute
 {
@@ -20,6 +21,8 @@ class MatchedRoute
      */
     public function handle($request, Closure $next)
     {
+        $this->writeDebugInfo();
+
         $route = $request->route();
         $matchedRoute = $this->getMatchedRouteInto($route);
         $matchedCallbacks = [];
@@ -60,5 +63,38 @@ class MatchedRoute
         ];
 
         return array_filter($matchedRoute);
+    }
+
+    public function terminate($request, $response)
+    {
+        if (! ($debug = app()['heyman_reaction_is_happened_in_debug'] ?? false)) {
+            return;
+        }
+
+        $this->writeForDebugbar($debug);
+
+        if (is_a($response, RedirectResponse::class)) {
+            $s = session();
+            $s->flash('heyman.debug.info', $debug);
+            $s->reflash();
+            $s->save();
+        }
+    }
+
+    private function writeDebugInfo()
+    {
+        if ($debug = session()->get('heyman.debug.info')) {
+            $this->writeForDebugbar($debug);
+        }
+    }
+
+    /**
+     * @param bool $debug
+     */
+    private function writeForDebugbar($debug)
+    {
+        resolve('heyman.debugger')->addMessage('HeyMan Rule Matched in file: ');
+        resolve('heyman.debugger')->addMessage($debug[0].' on line: '.$debug[1]);
+        resolve('heyman.debugger')->addMessage($debug[2]);
     }
 }
